31b680ae58c880ae7f560bd9da6b7b2d93ad2928
Katarzyna Berbeka <katarzyna.berbeka@gmail.com>
2018-06-17 10:03:59 +0200

    script playing

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("elo");
+
+/*function add7() {
+  let num1 = prompt("Your number?");
+  Number(num1);
+  let sum = num1 + 7;
+  return sum;
+  console.log(sum);
+}
+
+console.log(add7());*/
+
+function add7b(x) {
+  let result = x + 7;
+  console.log(result);
+}
+
+add7b(2);
+
+let add7c = y => y + 7;
+console.log(add7c(3));
+
+let multiply = (a, b) => a * b;
+console.log(multiply(7, 8));
+
+/*function capitalize() {
+  let str = prompt("Tell me a phrase");
+  let strLowered = str.toLowerCase();
+  console.log(strLowered);
+  let modifiedString = strLowered.charAt(0).toUpperCase() + strLowered.slice(1);
+  console.log(modifiedString);
+}
+
+console.log(capitalize());*/
+
+function lastLetter() {
+  let str2 = prompt("Tell me a word");
+  let str2Length = str2.length;
+  let result = str2.charAt(str2Length - 1);
+  console.log(result);
+}
+
+console.log(lastLetter());
