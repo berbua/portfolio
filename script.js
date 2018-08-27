d47694cba2b93199359ca33da25588bab5e7b303
berbua <katarzyna.berbeka@gmail.com>
2018-08-27 09:22:48 +0200

    General refactor

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// Mobile menu
-
-const mobileMenu = () => {
-  const burger = document.getElementById("js-mobile-menu");
-  const menu = document.querySelector(".main-nav-list");
-  const links = menu.querySelectorAll("a");
-  burger.addEventListener("click", function() {
-    menu.classList.toggle("open");
-    links.forEach(link =>
-      link.addEventListener("click", function() {
-        menu.classList.remove("open");
-      })
-    );
-  });
-};
-
-mobileMenu();
-
 // Sticky menu
-
 const stickyMenu = () => {
   window.onscroll = function() {
     stickyScroll();
@@ -35,71 +16,86 @@ const stickyMenu = () => {
 
 stickyMenu();
 
-// Slideshow
-
-// autoplay
-
-const slideShow = () => {
-  let playing = true;
-  let slideshowInterval;
-
-  function slideShowActive() {
-    if ((playing = true)) {
-      slideshowInterval = setInterval(showNextSlide, 3500);
-    }
-  }
-  slideShowActive();
+// Mobile menu
+const mobileMenu = () => {
+  const burger = document.getElementById("js-mobile-menu");
+  const menu = document.querySelector(".main-nav-list");
+  const links = menu.querySelectorAll("a");
+  burger.addEventListener("click", function() {
+    menu.classList.toggle("open");
+    links.forEach(link =>
+      link.addEventListener("click", function() {
+        menu.classList.remove("open");
+      })
+    );
+  });
 };
 
-slideShow();
+mobileMenu();
 
-// pins
+// Photos navigation
 
+const pins = document.querySelectorAll(".pin");
 let currentNumber = 1;
 document.querySelector("#slide" + currentNumber).classList.add("show");
 document.querySelector("#pin" + currentNumber).classList.add("selected");
 
-function selectPin(newNumber) {
+const selectPin = newNumber => {
   document.querySelector(".selected").classList.remove("selected");
   document.querySelector("#pin" + newNumber).classList.add("selected");
   currentNumber = newNumber;
-}
+};
 
-function showSlide(newNumber) {
+const showSlide = newNumber => {
   document.querySelector(".show").classList.remove("show");
   document.querySelector("#slide" + newNumber).classList.add("show");
-  currentNumber = newNumber;
   selectPin(newNumber);
-}
+  currentNumber = newNumber;
+};
 
-for (let numerPin = 1; numerPin < 5; numerPin++) {
-  document
-    .querySelector("#pin" + numerPin)
-    .addEventListener("click", function() {
-      showSlide(currentNumber);
-    });
-}
+pins.forEach(pin => {
+  pin.addEventListener("click", function() {
+    showSlide(this.id.slice(3));
+  });
+});
 
-// next, previous slide
+// next, previous slide arrows
 
-function showNextSlide() {
+const showNextSlide = () => {
   let newNumber = currentNumber + 1;
   if (newNumber > 5) {
     newNumber = 1;
   }
   showSlide(newNumber);
   currentNumber = newNumber;
-}
+};
 
 document.querySelector("#next").addEventListener("click", showNextSlide);
 
-function showPreviousSlide() {
+const showPreviousSlide = () => {
   let newNumber = currentNumber - 1;
   if (newNumber < 1) {
     newNumber = 5;
   }
   showSlide(newNumber);
   currentNumber = newNumber;
-}
+};
 
 document.querySelector("#prev").addEventListener("click", showPreviousSlide);
+
+// Photos slideshow
+
+// autoplay
+const slideShow = () => {
+  let playing = true;
+  let slideshowInterval;
+
+  function slideShowActive() {
+    if ((playing = true)) {
+      slideshowInterval = setInterval(showNextSlide, 4000);
+    }
+  }
+  slideShowActive();
+};
+
+slideShow();
